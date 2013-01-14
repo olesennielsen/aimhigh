86191b0ad71590fd786df20bece70dd6628bfeb1
Anders Konring Olesen <anders.konring@gmail.com>
2013-01-14 18:39:26 +0100

    Online mailing system:

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.11'
 gem 'heroku'
-gem 'sqlite3'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,23 @@ Aimhigh::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'aimhigh.herokuapp.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "ourworkouts.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "olesennielsen@gmail.com",
+    password: "evaBetty"
+  }
 
   # Enable threaded mode
   # config.threadsafe!
