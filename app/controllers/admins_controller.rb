551495ca87debca54d34f53a978cb4fd975e1069
Anders Konring Olesen <anders.konring@gmail.com>
2013-09-08 16:32:51 +0200

    Sorts after name in admins athlete list

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,7 @@
     load_and_authorize_resource
     def index
       @search = Athlete.search(params[:q])
-      @athletes = @search.result(:distinct => true)
+      @athletes = @search.result(:distinct => true).sort_by { |athlete| athlete.name || athlete.email}
     end
     
     def show
