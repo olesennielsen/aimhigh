725b6a41ab4690db27c30331c916de0f8d334818
Anders Konring Olesen <anders.konring@gmail.com>
2013-11-14 16:02:37 +0100

    admin#index sorting, bootstrap up-to-date

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,12 @@
     load_and_authorize_resource
     def index
       @search = Athlete.search(params[:q])
-      @athletes = @search.result(:distinct => true).sort_by { |athlete| athlete.name || athlete.email}
+      puts "This is some search object stuff: #{@search.sorts.empty?}"
+      if @search.sorts.empty?
+        @athletes = Athlete.all.sort_by{|a| a.name || a.email}
+      else
+        @athletes = @search.result(:distinct => true)
+      end
     end
     
     def show
